26de98a8bd77d7c4625c1a190e7fa02bad289c4a
Clément Brodu <clement.brodu@gmail.com>
2024-04-02 10:53:33 +0200

    bump: 1.0.2

---
+++ PCTTools/Model/UnionInterfaces.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PCTTools.Model
+{
+    internal class UnionInterfaces
+    {
+        public List<string> Interfaces { get; set; } = new List<string>();
+        public List<string> Usages { get; set; } = new List<string>();
+    }
+}
